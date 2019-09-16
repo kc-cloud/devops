13d6d6eaffbbf65385ad647833c0622170f049d0
kannan <kannan@kc-cloud.com>
2019-09-16 06:12:55 -0400

    added host to the python container

--- microservices/python/run-container.sh
+++ microservices/python/run-container.sh
@@ -2,6 +2,6 @@ docker run -d -p 5000:5000 \
     -e db_username=root \
     -e db_password=welcome1 \
     -e db_name=DEVOPS_USERS \
-    -e db_host=devops-mysql \
+    -e db_host=172.17.0.2 \
     --name devops-python \
     devops-python
